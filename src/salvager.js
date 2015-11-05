4210a566c1bf3caad1a12b4c8439512f53da5fc9
tanem <tane.morgan@gmail.com>
2015-11-05 17:03:48 +1300

    Add code docs (fixes #3)

--- src/salvager.js
+++ src/salvager.js
@@ -1,5 +1,62 @@
 import clamp from 'clamp';
 
+/**
+ * Reuse row elements when displaying large datasets. This is beneficial to
+ * performance since the number of row elements rendered to the DOM is fixed,
+ * no matter how big the dataset is. 
+ *
+ * The initial setup involves rendering the required number of row elements
+ * according to `bufferSize`, and adding a spacer element with a given height
+ * in order to set the scrollbar correctly.
+ *
+ * So if `data` is `['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']`, and
+ * `bufferSize` is `3`:
+ *
+ * ```
+ *   +--------+
+ * 0 | Item 1 |
+ *   +--------+
+ * 1 | Item 2 |
+ *   +--------+
+ * 2 | Item 3 |
+ *   +--------+
+ * 3
+ * 
+ * 4
+ * ```
+ *
+ * Then when the dataset is scrolled, row elements are transitioned from the old
+ * buffer to the new. The new buffer position is based on the scroll position
+ * of the target.
+ *
+ * `i`, `j` and `k` below represent the variable names used to reference the
+ * significant parts of the old and new buffers. In this case, the row element
+ * that currently displays `data[0]` will be reused to render `data[3]`.
+ *
+ * The transition of a row element involves a CSS transform to the new position,
+ * and an update of the row element in order to render the data at that point:
+ *
+ * ```
+ *   +------------+
+ * 0 | Item 1 (k) |
+ *   +------------+     +------------+
+ * 1 | Item 2     |     | Item 2 (i) |
+ *   +------------+ --> +------------+
+ * 2 | Item 3     |     | Item 3     |
+ *   +------------+     +------------+
+ * 3                    | Item 4 (j) |
+ *                      +------------+
+ * 4
+ * ```
+ *
+ * @param {HTMLElement} target The element to attach to.
+ * @param {Array} data The data to be displayed.
+ * @param {Number} bufferSize The number of rendered row elements.
+ * @param {Function} [buildRow] Defines how row elements should be initially rendered.
+ * @param {Function} [updateRow] Defines how existing row elements should be updated.
+ * @returns {Object}
+ * @access public
+ */
 export default function salvager({
   target,
   data,
@@ -89,6 +146,11 @@ export default function salvager({
 
   return {
 
+    /**
+     * Destroy `salvager`.
+     *
+     * @access public
+     */
     destroy() {
       target.removeEventListener('scroll', scrollHandler);
       target.removeChild(container);
